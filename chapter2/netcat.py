b4038c50c88b637c5a693722a6782c3f8c4d3039
nimitzufo <nimitzufo@protonmail.com>
2025-01-10 14:18:25 -0300

    updating netcat

--- chapter2/netcat.py
+++ chapter2/netcat.py
@@ -13,6 +13,48 @@ def execute(cmd):
     output = subprocess.check_output(shlex.split(cmd),stderr=subprocess.STDOUT)
     return output.decode()
 
+class NetCat:
+    def __init__(self, args, buffer=None):
+        self.args = args
+        self.buffer = buffer
+
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.setsocopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    def run(self):
+        if self.args.listen:
+            self.listen()
+        else:
+            self.send()
+
+    def send(self):
+        self.socket.connect((self.args.target, self.args.port))
+        if self.buffer:
+            self.socket.send(self.buffer))
+
+        try:
+            while True:
+                recv_len=1
+                response=''
+                while recv_len:
+                    data = self.socket.recv(4096)
+                    recv_len = len(data)
+                    response += data.decode()
+                    if recv_len < 4096:
+                        break
+                if response:
+                    print(response)
+                    buffer = input('>')
+                    buffer += '\n'
+
+                    self.socket.send(buffer.encode())
+
+        except KeyboardInterrupt:
+            print('User terminated.')
+            self.socket.close()
+            sys.exit()
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumetParser(description='BHP Net Tool',formatter_class=argparse.RawDescriptionHelpFormatter,epilog=textwrap.dedent('''Example: netcat.py -t 192.168.1.108 -p 5555 -l -c # command shell netcat.py -t 192.168.1.108 -p 5555 -l -u=mytest.txt #upload to file netcat.py -t 192.168.1.108 -p 5555 -l -e=\"cat /etc/passwd\" # execute command'''))
     parser.add_argument('-c', '--command', action='store_strue', help='command shell')
